2d184768fe65e0ad68be7638ff0a1ffa4d523fe7
PHILIP <philophilo037@gmail.com>
2022-08-29 15:58:16 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    addTask(e.target.addTask.value);
+    form.reset();
+  })
 });
+
+function addTask(e){
+  var li = document.createElement('li')
+  var btn = document.createElement('button')
+  btn.innerHTML = 'Delete'
+  btn.style.backgroundColor = 'red'
+  btn.style.color = 'white'
+  btn.addEventListener('click', handleDelete)
+  li.textContent = `${e} `
+  const parent = document.getElementById('tasks')
+  parent.appendChild(li)
+  li.appendChild(btn)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
